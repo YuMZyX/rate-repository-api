2e24b3f871610bf6b9a27678b7099985ab0ed0ee
YuMZyX <joni1koskinen@gmail.com>
2023-11-16 13:16:18 +0200

    Database change sqlite -> pg

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,22 @@
 require('dotenv').config();
+const DATABASE_URL = process.env.DATABASE_URL;
+const PGHOST = process.env.PGHOST;
+const PGPORT = process.env.PGPORT;
+const PGUSER = process.env.PGUSER;
+const PGDATABASE = process.env.PGDATABASE;
+const PGPASSWORD = process.env.PGPASSWORD;
 
-const FILENAME = process.env.DATABASE_FILENAME || 'database.sqlite';
+//const FILENAME = process.env.DATABASE_FILENAME || 'database.sqlite';
 
 module.exports = {
-  client: 'sqlite3',
+  client: 'pg',
   connection: {
-    filename: FILENAME,
+    connectionString: DATABASE_URL,
+    host: PGHOST,
+    port: PGPORT,
+    user: PGUSER,
+    database: PGDATABASE,
+    password: PGPASSWORD,
   },
-  useNullAsDefault: true,
+  //useNullAsDefault: true,
 };
