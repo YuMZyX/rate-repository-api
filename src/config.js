647bfc6f359f590ec5f8083f96a177d01d943275
YuMZyX <joni1koskinen@gmail.com>
2023-11-20 18:39:11 +0200

    Fixes to port configuration

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ import knexfile from '../knexfile';
 
 dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
 
-export const API_PORT = process.env.PORT || 5000;
+export const API_PORT = process.env.API_PORT || 5000;
 
 export const APOLLO_PORT = process.env.PORT || 4000;
 
